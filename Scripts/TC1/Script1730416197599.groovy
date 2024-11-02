200a7ebcdded356ea8d1a52457d177705a7301cb
kazurayam <kazuaki.urayama@gmail.com>
2024-11-02 10:32:19 +0900

    changed TC1 a bit

--- Scripts/TC1/Script1730416197599.groovy
+++ Scripts/TC1/Script1730416197599.groovy
@@ -11,29 +11,36 @@ import my.TestHelper
 // Sample URL used in the Selenium offical doc at https://www.selenium.dev/documentation/webdriver/interactions/windows/
 String url = "https://www.selenium.dev/selenium/web/window_switching_tests/page_with_frame.html"
 
-TestObject anchorOpenNewWindow = TestHelper.makeTestObject('Open new window', "//a[@id='a-link-that-opens-a-new-window']")
+TestObject anchorOpenNewTab = TestHelper.makeTestObject('Open new window', "//a[@id='a-link-that-opens-a-new-window']")
+TestObject divSimple = TestHelper.makeTestObject('div simple', "/html/body/div[text()='Simple page with simple test.']")
 
+// Let's work on the 1st Chrme Browser
 WebUI.openBrowser('')
 WebDriver driver1 = DriverFactory.getWebDriver()
 TestHelper.layoutWindow(driver1, 1024, 400, 20, 20)
 WebUI.navigateToUrl(url)
-WebUI.click(anchorOpenNewWindow)
+WebUI.click(anchorOpenNewTab)
 WebUI.delay(1)
 WebUI.switchToWindowIndex(1)
-// make sure that we are looking at the 2nd tab, not the 1st tab
-WebUI.verifyElementNotPresent(anchorOpenNewWindow, 10)
+// make sure that we are looking at the 2nd tab
+WebUI.verifyElementPresent(divSimple, 10)
+// make sure that we are NOT looking at the 1st tab
+WebUI.verifyElementNotPresent(anchorOpenNewTab, 10)
 
+// Let's work on the 2nd Chrome Browser
 ChromeOptions options = new ChromeOptions()
 options.setExperimentalOption("excludeSwitches", ["enable-automation"]);
 WebDriver driver2 = new ChromeDriver(options)
 TestHelper.layoutWindow(driver2, 1024, 400, 60, 440)
 DriverFactory.changeWebDriver(driver2)
 WebUI.navigateToUrl(url)
-WebUI.click(anchorOpenNewWindow)
+WebUI.click(anchorOpenNewTab)
 WebUI.delay(1)
 WebUI.switchToWindowIndex(1)
-// make sure that we are looking at the 2nd tab, not the 1st tab
-WebUI.verifyElementNotPresent(anchorOpenNewWindow, 10)
+// make sure that we are looking at the 2nd tab
+WebUI.verifyElementPresent(divSimple, 10)
+// make sure that we are NOT looking at the 1st tab
+WebUI.verifyElementNotPresent(anchorOpenNewTab, 10)
 
 driver1.quit()
 driver2.quit()
